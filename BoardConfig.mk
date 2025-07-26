b5db5391ea7a686a7104f93a276876446729298b
R0rt1z2 <me@r0rt1z2.com>
2025-07-26 19:01:58 +0200

    kobe2: Define Bootloader and Platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,12 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 TARGET_USES_64_BIT_BINDER := true
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := KOB2
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6765
+
 # Inherit the proprietary files
 include vendor/huawei/kobe2/BoardConfigVendor.mk
