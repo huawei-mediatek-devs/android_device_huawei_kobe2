f9b4c3ae27fbb24fcb8a7e53ff6cc0348dfbb13b
R0rt1z2 <me@r0rt1z2.com>
2025-07-26 18:58:45 +0200

    kobe2: Define Architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,15 @@
 
 DEVICE_PATH := device/huawei/kobe2
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/huawei/kobe2/BoardConfigVendor.mk
