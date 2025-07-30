f488a7c85251204635348ecf604180b36cacd0a7
R0rt1z2 <me@r0rt1z2.com>
2025-07-30 21:28:01 +0200

    kobe2: Configure Bluetooth stack

--- device.mk
+++ device.mk
@@ -55,6 +55,11 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhwbinder
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
