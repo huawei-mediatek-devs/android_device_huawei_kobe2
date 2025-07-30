87ee7962f3ff25cf45d22652e6d4517f7553cb5f
Bruno Martins <bgcngm@gmail.com>
2025-07-30 21:30:27 +0200

    kobe2: Build Health 2.1 HAL

--- device.mk
+++ device.mk
@@ -89,6 +89,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
     libkeymaster3device.vendor \
