8b7e8f32ae1692ef521dbf2f282fb44ceac808fb
bengris32 <bengris32@protonmail.ch>
2025-07-31 19:31:57 +0200

    kobe2: Build `android.hidl.{allocator,memory}@1.0` libs

--- device.mk
+++ device.mk
@@ -6,6 +6,10 @@
 
 # Binder
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0.vendor \
+    android.hidl.memory@1.0.vendor
+
+PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder.vendor \
